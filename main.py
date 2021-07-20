e7ce5e622870999759a6fd515877e22284966ce9
Miguel_Arriaga <arriamgu@schaeffler.com>
2021-07-20 15:29:36 -0500

    Se agregan variables para recolectar las coordenadas de las esquinas de los rectangulos. Fucnional. Sigue pendiente el TODO

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from point_rectangles import Point_Rectangles
 
 def run():
-    rectangle = Point_Rectangles(2)
+    rectangle = Point_Rectangles(5)
 
     rectangle.capture_corners()
+
+    print(f"Coordenadas de los rectangulos: {rectangle.get_rectangles_coordenates}")
 
 
 if __name__ == "__main__":
--- point_rectangles.py
+++ point_rectangles.py
@@ -1,5 +1,6 @@
 from pynput.mouse import Controller
 
+
 class Point_Rectangles:
     """This class allows to point and get the coordenates of corner of rectangles pointed by the mouse on the screen.
     """
@@ -7,10 +8,12 @@ class Point_Rectangles:
     #region <Variables>
 
     _capture_started = False
+    _rectangles_captured = False
     _origin_point = (0, 0)
     _current_position = (0, 0)
 
     _rectangle_coordenates = []
+    _rectangle_corners_coordinates = [None] * 4
 
     _rectangles_to_capture = int
     _rectangles_registered = int
@@ -22,7 +25,32 @@ class Point_Rectangles:
 
     #region <Attributes>
 
+    @property
+    def get_last_rectangle_coordenates(self):
+        """Retrieves the last four rectangle coordenates
 
+        Returns:
+            [list]: Last four coordenates of the last captured rectangle
+        """
+        return self._rectangle_corners_coordinates
+
+    @property 
+    def get_rectangles_coordenates(self):
+        """Retrieves all the captured rectangles coordenates 
+
+        Returns:
+            [list]: Coordinate lists
+        """
+        return self._rectangle_coordenates
+
+    @property
+    def get_rectangles_registered(self):
+        """Retrieves the number of rectangles registered
+
+        Returns:
+            [int]: Number of rectangles registered
+        """
+        return self._rectangles_registered
 
     #endregion
 
@@ -31,8 +59,7 @@ class Point_Rectangles:
     def __init__(self, rectangles_to_capture: int):
         self._print_instruction()
 
-        global mouse_ctrl
-        mouse_ctrl = Controller()
+        self.mouse_ctrl = Controller()
         
         self._rectangle_corner_count = 0
         self._rectangles_registered = 0
@@ -64,22 +91,38 @@ class Point_Rectangles:
         Args:
             coordenate ([tupple]): Rectangle's corner coordinate
         """
-        if self._capture_started == True and self._rectangles_registered != self._rectangles_to_capture:
-            self._rectangle_coordenates.append(coordenate)
+        if self._capture_started and not self._rectangles_captured:
+            # Asignación de nueva coordenada de esquina de rectangulo
+            self._rectangle_corners_coordinates[self._rectangle_corner_count] = coordenate
 
             self._rectangle_corner_count += 1
 
-            if self._rectangle_corner_count > 3:
+            if self._rectangle_corner_count >= 4:
                 self._rectangles_registered += 1
                 self._rectangle_corner_count = 0
+
+                # Se agrega self._rectangle_corners_coordinates[:] de esta forma debido a que si se hace directamente,
+                # python copia la dirección del apuntador y al borrar la lista "_rectangle_corners_coordinates" tambien
+                # lo hace la lista "_rectangle_coordenates"
+                self._rectangle_coordenates.append(self._rectangle_corners_coordinates[:])
+
+                if self._rectangles_registered == self._rectangles_to_capture: self._rectangles_captured = True
+        
+        elif self._capture_started and self._rectangles_captured:
+            print(f"> Corner capture completed, no more corners were added...")
+
         else:
+            print(f"> Corner capture not ready...")
             self._capture_started = False
+
 
     #enderegion
 
     #region <Public Methods>
 
     def capture_corners(self):
+        """Initializes the rectangles' corners capture
+        """
         while True:
             cmd = input("> Command: ")
 
@@ -88,23 +131,29 @@ class Point_Rectangles:
                 
                 print("> Capture started...")
 
-            if (cmd == "-f" or cmd == "--finish") and self._capture_started == True:
+            if (cmd == "-f" or cmd == "--finish") and self._capture_started:
                 self._capture_started = False
                 
                 print("> Capture finished...")
             
-            if (cmd == "-o" or cmd == "--origin") and self._capture_started == True:
-                self._origin_point = mouse_ctrl.position
+            if (cmd == "-o" or cmd == "--origin") and self._capture_started:
+                self._origin_point = self.mouse_ctrl.position
                 
                 print(f"> New origin setted: {self._origin_point} => (0, 0)")
             
-            if cmd == "" and self._capture_started == True:
-                self._current_position = tuple(map(lambda current_pos, origin: current_pos - origin, mouse_ctrl.position, self._origin_point))
+            if cmd == "" and self._capture_started and not self._rectangles_captured:
+                self._current_position = tuple(map(lambda current_pos, origin: current_pos - origin, self.mouse_ctrl.position, self._origin_point))
                 
                 self._add_corner(self._current_position)
 
-                print(f"> New corner point added: {self._current_position} relative to origin ({self._origin_point}) {self._rectangles_registered} / {self._rectangles_to_capture}")
+                print(f"> New corner point added: {self._current_position} relative to origin ({self._origin_point})\n\tRectangles: {self._rectangles_registered} / {self._rectangles_to_capture}")
+
+                # print(f"Corners: {self._rectangle_corners_coordinates}\nRectangle: {self._rectangle_coordenates}")
             
+            if cmd == "" and self._capture_started and self._rectangles_captured:
+                print(f"> Number of rectangles reached: {self._rectangles_registered} out of {self._rectangles_to_capture}")
+                break
+                
             if (cmd == "-q" or cmd == "--quit"):
                 # TODO: Crear una funcion que limpie todos los elementos y devuelva un array de ceros
 
